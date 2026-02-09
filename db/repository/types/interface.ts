1c32d555bc4e8a76345d8ff6802a4b33783e8954
radooo <radoslav.a.tsvetanov.2021@elsys-bg.org>
2026-02-09 23:49:29 +0200

    db

--- db/repository/types/interface.ts
+++ db/repository/types/interface.ts
@@ -1,8 +1,16 @@
 import type z from "zod/v4";
 import { songSchema } from "../../schema";
+import type { TypeSafeOmit } from "@blazyts/better-standard-library";
 
+type SongQuery = {
+    name: {is: string} | {contains: string}
+    tags: {hasTagOfType?: string, valueOfTagIs?: string}
+    author: string
+}
 
 export interface IRepo {
-    createSong(song:  z.infer<typeof songSchema>): Promise<void>
-    
+    createSong(song:  TypeSafeOmit<z.infer<typeof songSchema>, "audioHash">): Promise<void>
+    get(query: SongQuery): Promise<>
+    getUsingAi(prompt: string)
+    getAll()
 }
